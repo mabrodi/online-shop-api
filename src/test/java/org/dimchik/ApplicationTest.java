9db7d155f1e6d420ee35692154de010fa440ca57
dimaperehuda <mabrodi7@gmail.com>
2025-08-17 18:15:34 +0300

    add main test

---
+++ src/test/java/org/dimchik/ApplicationTest.java
@@ -0,0 +1,13 @@
+package org.dimchik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class ApplicationTest {
+    @Test
+    void contextLoads() {
+    }
+}
